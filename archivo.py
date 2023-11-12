13c450d63dea208f99f3f28a4a924ac0f573780b
josuesauca <69443045+josuesauca@users.noreply.github.com>
2023-11-12 18:17:14 -0500

    Update archivo.py
    cambios

--- archivo.py
+++ archivo.py
@@ -2,20 +2,76 @@ import tkinter as tk
 import cv2
 import numpy as np
 import os
-from PIL import Image, ImageDraw, ImageGrab
+from tkinter import messagebox, filedialog
 
-#Libreria para reconocer texto de imagenes
-import aspose.ocr as ocr
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from PIL import Image, ImageDraw, ImageGrab
+import torchvision.transforms as transforms
 
 #Librerias para mnist
-
 import torch
 import torch.nn as nn
 from torchvision import datasets, transforms
 
+#Neurona para realizar el entrenamiento respectivo
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
 
-mnist = datasets.MNIST(root='.', train=True, download=True)
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
+if not os.path.isfile('model.pth'):
+    # Descargar el conjunto de datos y entrenar el modelo
+	train_dataset = datasets.MNIST(root='data', train=True, transform=transforms.ToTensor(), download=True)
+	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+	model = NeuralNetwork().to(device)
+
+	loss_fn = nn.CrossEntropyLoss()
+	optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+	def train(dataloader, model, loss_fn, optimizer):
+	    size = len(dataloader.dataset)
+	    model.train()
+	    for batch, (X, y) in enumerate(dataloader):
+	        X, y = X.to(device), y.to(device)
+
+	        # Compute prediction error
+	        pred = model(X)
+	        loss = loss_fn(pred, y)
+
+	        # Backpropagation
+	        optimizer.zero_grad()
+	        loss.backward()
+	        optimizer.step()
+
+	        if batch % 100 == 0:
+	            loss, current = loss.item(), batch * len(X)
+	            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+	epochs = 16
+	for t in range(epochs):
+	    print(f"Epoch {t+1}\n-------------------------------")
+	    train(train_loader, model, loss_fn, optimizer)
+	print("Done!")
+
+	model.state_dict()
+	torch.save(model.state_dict(), "model.pth")
 
 points_list = []
 
@@ -30,12 +86,6 @@ def clear_canvas():
     canvas.delete('all')
     points_list.clear()
 
-# create a button to save the canvas object
-def save_canvas():
-    canvas.postscript(file="drawing.eps", colormode="color")
-    img = Image.open("drawing.eps")
-    img.save("drawing.png", "png")
-
 def capture_drawing():
     if len(points_list) > 0:
         x_coords, y_coords = zip(*points_list)
@@ -43,30 +93,45 @@ def capture_drawing():
         max_x, max_y = max(x_coords), max(y_coords)
         width = max_x - min_x
         height = max_y - min_y
-        image = Image.new('L', (width+20, height+20), 'white')  
+        image = Image.new('L', (width+40, height+40), 'white')  # aumentar el tamaño de la imagen en 40 píxeles en cada dimensión
         draw = ImageDraw.Draw(image)
         for i in range(len(points_list)-1):
             x1, y1 = points_list[i]
             x2, y2 = points_list[i+1]
-            draw.line((x1 - min_x+10, y1 - min_y+10, x2 - min_x+10, y2 - min_y+10), fill='black', width=10)  
+            draw.line((x1 - min_x+20, y1 - min_y+20, x2 - min_x+20, y2 - min_y+20), fill='black', width=10)  # agregar un desplazamiento de 20 píxeles a cada punto dibujado
         filename = 'drawing.jpg'
         image.save(os.path.join(os.getcwd(), filename), dpi=(300, 300))
-        #print('Drawing saved as', filename)
-        image.show()
+
+        verificar_modelo(filename)
 
 
-def reconocer_texto():
+def select_image():
+    file_path = filedialog.askopenfilename()
+    verificar_modelo(file_path)
 
-	for i in range(len(mnist)):
-	    if (mnist[i][1] < 10 and len(images) < num_images and bandera) :
-	        #images.append(mnist[i][0]) sirve para obtener la imagen
-	        #labels.append(mnist[i][1]) sirve para obtener el nombre en entero
-	        if mnist[i][1] == numero:
-	           num_obtenido = mnist[i][1]
-	           bandera = False
+def verificar_modelo(file):
+	test_image = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
 
-	print('Es el numero: ',num_obtenido)
+	# Format Image
+	img_resized = cv2.resize(test_image, (28, 28), interpolation=cv2.INTER_LINEAR)
+	img_resized = cv2.bitwise_not(img_resized)
 
+	# Resize the image to 28x28
+	transform = ToTensor()
+
+	img_tensor = transform(img_resized)
+	img_tensor = img_tensor.unsqueeze(0)
+
+	# Load the model
+	model = NeuralNetwork()
+	model.load_state_dict(torch.load("model.pth"))
+
+	# Evaluate the model
+	model.eval()
+	with torch.no_grad():
+	    output = model(img_tensor)
+	    _, predicted = torch.max(output, 1)
+	messagebox.showinfo("Número ingresado", f"El número ingresado es: {predicted.item()}")
 
 window = tk.Tk()
 window.title('Canvas')
@@ -90,12 +155,13 @@ canvas = tk.Canvas(window, background="white", width=window_width, height=window
 canvas.place(relx=0.5, rely=0.5, anchor='center')
 canvas.bind('<B1-Motion>', draw_on_canvas)
 
-clear_button = tk.Button(window, text="Clear", command=clear_canvas)
+clear_button = tk.Button(window, text="Limpiar Pantalla", command=clear_canvas)
 clear_button.place(relx=0.75, rely=1.0, anchor='s')
 
-#capture_button = tk.Button(window, text="Capture", command=reconocer_texto)
-capture_button = tk.Button(window, text="Capture", command=capture_drawing)
+capture_button = tk.Button(window, text="Identificar Imagen", command=capture_drawing)
 capture_button.place(relx=0.25, rely=1.0, anchor='s')
 
+select_button = tk.Button(window, text="Seleccionar Imagen", command=select_image)
+select_button.place(relx=0.50, rely=1.0, anchor='s')
 
 window.mainloop()
